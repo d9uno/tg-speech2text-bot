ae485c9691f5a9a4537d0d27dfc5ea4f5e1076ec
d9uno <d9uno@mail.ru>
2022-10-28 12:54:57 +0300

    add start listening code

--- src/main/java/me/d9uno/tg_speech2text_bot/Main.java
+++ src/main/java/me/d9uno/tg_speech2text_bot/Main.java
@@ -1,6 +1,16 @@
 package me.d9uno.tg_speech2text_bot;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            var botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MessageHandler(args[0], args[1]));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
