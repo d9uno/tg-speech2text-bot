97295fa373b03749e893bb84f1b8b0b75f4e2a60
d9uno <d9uno@mail.ru>
2022-10-28 11:28:43 +0300

    add download and convert audio file code

--- src/main/java/me/d9uno/tg_speech2text_bot/MessageHandler.java
+++ src/main/java/me/d9uno/tg_speech2text_bot/MessageHandler.java
@@ -1,11 +1,14 @@
 package me.d9uno.tg_speech2text_bot;
 
+import java.nio.file.Path;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MessageHandler extends TelegramLongPollingBot {
+    private final MediaConverter mediaConverter = new MediaConverter();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -18,6 +21,10 @@ public class MessageHandler extends TelegramLongPollingBot {
                 filePathOnServer = execute(new GetFile(receivedMessage.getVoice().getFileId())).getFilePath();
             else if (receivedMessage.hasVideoNote())
                 filePathOnServer = execute(new GetFile(receivedMessage.getVideoNote().getFileId())).getFilePath();
+
+            var wavFile = mediaConverter.convertAudio(
+                downloadFile(filePathOnServer, Path.of("./data", receivedMessage.getChatId().toString(), filePathOnServer).toFile()),
+            "wav", "pcm_s16le", 128000, 1, 16000);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
