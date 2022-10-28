f813b0b6d10ca8c0ca16333cd1e1ddfeae8f35a2
d9uno <d9uno@mail.ru>
2022-10-28 10:59:49 +0300

    add changeFileExtension method

--- src/main/java/me/d9uno/tg_speech2text_bot/MediaConverter.java
+++ src/main/java/me/d9uno/tg_speech2text_bot/MediaConverter.java
@@ -1,7 +1,14 @@
 package me.d9uno.tg_speech2text_bot;
 
+import java.io.File;
+
 import ws.schild.jave.Encoder;
 
 public class MediaConverter {
     private final Encoder encoder = new Encoder();
+
+    private File changeFileExtension(File sourceFile, String newExtension) {
+        var index = sourceFile.getPath().lastIndexOf(".") + 1;
+        return new File(sourceFile.getPath().substring(0, index) + newExtension);
+    }
 }
