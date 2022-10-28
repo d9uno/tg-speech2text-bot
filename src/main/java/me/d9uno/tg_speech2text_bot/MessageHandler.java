c641400dedd1bf3849e4048a7b35196fc3e508a6
d9uno <d9uno@mail.ru>
2022-10-28 10:37:41 +0300

    add vars, check message type

--- src/main/java/me/d9uno/tg_speech2text_bot/MessageHandler.java
+++ src/main/java/me/d9uno/tg_speech2text_bot/MessageHandler.java
@@ -1,13 +1,26 @@
 package me.d9uno.tg_speech2text_bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MessageHandler extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
         if (!update.hasMessage()) return;
+        try {
+            var receivedMessage = update.getMessage();
+            var filePathOnServer = new String();
+
+            if (receivedMessage.hasVoice())
+                filePathOnServer = execute(new GetFile(receivedMessage.getVoice().getFileId())).getFilePath();
+            else if (receivedMessage.hasVideoNote())
+                filePathOnServer = execute(new GetFile(receivedMessage.getVideoNote().getFileId())).getFilePath();
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
