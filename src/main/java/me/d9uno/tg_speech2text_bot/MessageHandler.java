94befffeda25340996b0ef4447e619910826907f
d9uno <d9uno@mail.ru>
2022-10-28 10:31:45 +0300

    implement methods

--- src/main/java/me/d9uno/tg_speech2text_bot/MessageHandler.java
+++ src/main/java/me/d9uno/tg_speech2text_bot/MessageHandler.java
@@ -1,5 +1,22 @@
 package me.d9uno.tg_speech2text_bot;
 
-public class MessageHandler {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MessageHandler extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
     
 }
